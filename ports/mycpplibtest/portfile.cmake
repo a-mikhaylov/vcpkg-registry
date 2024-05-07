7604200dad4e236c6a93b07d5e5b6abb1282bafc
oldwizzard <lexarobotr2d2@gmail.com>
2024-05-07 19:33:54 +0300

    update head commit

--- ports/mycpplibtest/portfile.cmake
+++ ports/mycpplibtest/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO a-mikhaylov/my-c-lib
-    REF 5f55757
-    SHA512 fbdded5c275bb9947eac6d164c17e13fffdbd193e50f99fa0f620fa90d6a8874d82126250c8d034983fbefd27fd30276d9b9a3abd87e183d24f0413feb19a920    
+    REF 2105b76
+    SHA512 a8adf31496f4a2a9d82a43b178cc4bef001cf849c5283fbeea51b88e9a23514036ddaaea5a95b1d0bcfb23d1d8d3037037e781567f7060888bf22b1cd221c148    
     HEAD_REF main
 )
 
