696d6de9c17b2b191cb33c87647b41e6cf23caa7
oldwizzard <lexarobotr2d2@gmail.com>
2024-05-08 16:15:39 +0300

    update

--- ports/mycpplibtest/portfile.cmake
+++ ports/mycpplibtest/portfile.cmake
@@ -1,16 +1,20 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO a-mikhaylov/my-c-lib
-    REF 66cc8d2
-    SHA512 edc6376e605ca13602dc39a78db52772a830f01d830e025e7a6f37c7ff815419f03a9a1007b09dcc32b2f95f9ce34c1345775898412479f768b73c93b5440f3b    
+    REF 6a773da3a292b1cd598d77824d3517fd1c8039a4
+    SHA512 7976d84915e687df45bdd097e859f6a5c0a93a287f2f41a5e1f93b61c0289d160d746ea85a922c648514878973e20165fd94b7bd62682956f6dd89710ab6e60e
     HEAD_REF main
 )
 
 # set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
 
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/mycpplibtest" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/mycpplibtest" RENAME copyright)
+# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib" "${CURRENT_PACKAGES_DIR}/lib")
