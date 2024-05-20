dfa72920e706824572c334ee60ce6d6b4ffdffb0
oldwizzard <lexarobotr2d2@gmail.com>
2024-05-20 18:57:08 +0300

    update mycpplibtest commit

--- ports/mycpplibtest/portfile.cmake
+++ ports/mycpplibtest/portfile.cmake
@@ -1,8 +1,7 @@
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO a-mikhaylov/my-c-lib
-    REF ede70b4b376cb4a5cc0c1759282277f69b306332
-    SHA512 a3578a9350efb0ba85e7838d4102cfb72c520d38114964a516440b822e4043a4b5867b3e4d4439470012ba6afd52c1d3b5dece2f38e30ea256abd02b802882b0
+    URL git@github.com:a-mikhaylov/my-c-lib.git
+    REF 6e17ccb21c5b977c7769c99d17b1816a6306b7b6
     HEAD_REF main
 )
 
