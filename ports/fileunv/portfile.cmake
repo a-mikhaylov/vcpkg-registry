42c92d4fb1eb56cc58415e6ec787cce321337a2a
oldwizzard <lexarobotr2d2@gmail.com>
2024-05-23 19:50:19 +0300

    update fileunv

--- ports/fileunv/portfile.cmake
+++ ports/fileunv/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:IncartDev/crossplatform-file-dll.git
-    REF 5f13ccd2595ee7e1dfdefcd147f37f70b4c88385
+    REF 3fb36614493a595690bd548d02f4472d77ec20b3
     HEAD_REF man
 )
 
